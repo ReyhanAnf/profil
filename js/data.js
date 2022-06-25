166aa9547a9b76c815f281664b174262ce91cce1
Reyhan Andrea Firdaus <77052916+ReyhanAnf@users.noreply.github.com>
2022-06-26 05:48:50 +0700

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,42 @@
+const text_data = {
+    indonesia : {
+        hero: ['Hanya Seorang Pelajar','Ada cara mudah dan cara sulit. Bagian yang sulit adalah menemukan cara yang mudah',`Halo..Semoga baik disana!<br/>saya seorang pelajar laki laki yang sangat suka membangun web, sedikit mendesain apalagi mengolah data.`,
+                "Baca Tentang Reyhan Lebih Lanjut",],
+        nav:  ["Tentang saya","Skills", "Pengalaman","Projek", "Blog", "Kontak"],
+        exp: ["Mempunyai artikel yang membahas tentang wireless ",
+            "Lulusan kelas bootcamp Dicoding dibidang data dan pembelajaran mesin",
+            "Aktif berorganisasi",
+            "Telah dipercayai google untuk memasang iklan pada blog",
+        ],
+        project: ["Prediksi Berita","Ada",
+                "Ketika kamu bingung dengan artikel berita yang kamu baca, kamu bisa meminta bantuan mesin",
+                "Subtitel Otomatis","Segera",
+                "Menggunakan algoritma yang bisa merubah suara menjadi teks. lalu boom!Video mempunyai terjemahan",
+                "Segera","Segera",
+                "Punya ide gak? Kalo ada kamu bisa sampaikan lewat email!"],
+        blog : ['Kunjungi'],
+        contact : ["Kamu jangan sungkan untuk menghubungi saya. Karena saya akan berusaha selalu ada disini!"],
+        credit : ["Didesain & Dibangun oleh",
+                "Reyhan Andrea Firdaus"]
+    },
+    inggris : {
+        hero: ['Only a Student','There is an easy way and a hard way. The hard part is finding the easy way.',`Hello..Good luck there!<br/>I am a boy student who really likes to build web, few design especially processing data.`,
+                "Read More to About Reyhan",],
+        nav:  ["About Me","Skills", "Experience","Project", "Blog", "Contact"],
+        exp: ["Has article that discuss wireless",
+            "Active following the activity bootcamp",
+            "Active in organization",
+            "Google has trusted to place ads on blog",
+        ],
+        project: ["Predict news","on",
+                "when you are confused by the news you read, you can ask for machine help",
+                "Subtitle Otomatic","Coming soon",
+                "Using an algorithm that can convert voice to text. then boom!Video have translate",
+                "Coming soon","Coming soon",
+                "Do you have idea? If so, can you send it via email!"],
+        blog : ['Kunjungi'],
+        contact : ["Dont hesitate to contaxt me. Because i'll always try to always be there!"],
+        credit : ["Didesain & Dibangun oleh",
+                "Reyhan Andrea Firdaus"]
+    }
+}
